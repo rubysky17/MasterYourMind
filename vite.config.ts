6afe470b3e7b6fac9c7848492fb39ca11b690c9e
Dat Vo Manh <dat.vo@blueberrylabs.com>
2026-02-16 17:25:28 +0700

    feat: setup react pwa with vite and indexeddb

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: "auto",
+    
+    pwaAssets: {
+      disabled: true,
+    },
+
+    manifest: {
+      name: 'Master Your Mind',
+      short_name: 'Master Your Mind',
+      description: 'Master Your Mind',
+      theme_color: '#ffffff',
+      icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
